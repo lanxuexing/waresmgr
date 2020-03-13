46f5a8562028ab19d2be52bd38018035f45f45e8
lanxuexing <lanxuexing313wsr@163.com>
2020-03-13 17:18:14 +0800

    feat: home goods

---
+++ src/app/directives/category.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, Input, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appCategory]',
+})
+export class CateGoryDirective {
+    @Input() @HostBinding('style.background-color') categoryBackgroundColor = '#FAEFE3';
+    @Input() @HostBinding('style.color') categoryColor = '#CA516A';
+    @Input() @HostBinding('style.font-size') categoryFontSize = '0.8rem';
+    @Input() @HostBinding('style.padding') categoryPadding = '3px';
+}
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GridItemDirective } from './grid-item.directive';
-import { GridItemImageDirective } from './grid-item-image.directive';
-import { GridItemTitleDirective } from './grid-item-title.directive';
+import { GridItemDirective } from '@directives/grid-item.directive';
+import { GridItemImageDirective } from '@directives/grid-item-image.directive';
+import { GridItemTitleDirective } from '@directives/grid-item-title.directive';
+import { CateGoryDirective } from '@directives/category.directive';
+import { AvatarDirective } from '@directives/avatar.directive';
 
 const COMMON = [
   GridItemDirective,
   GridItemImageDirective,
   GridItemTitleDirective,
+  CateGoryDirective,
+  AvatarDirective,
 ];
 
 @NgModule({
