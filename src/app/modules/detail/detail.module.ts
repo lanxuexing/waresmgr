668dbaa0d4bb9d90101d4d5f92b863b0e25928a9
lanxuexing <lanxuexing313wsr@163.com>
2020-03-13 23:08:01 +0800

    feat: init order

--- src/app/modules/detail/detail.module.ts
+++ src/app/modules/detail/detail.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { DetailComponent } from '@modules/detail';
 import { DetailRoutingModule } from '@modules/detail/detail.routing';
 import { ComponentsModule } from '@components/components.module';
+import { OrderComponent } from '@modules/detail/order';
 
 const COMMON = [
-    DetailComponent
+    DetailComponent,
+    OrderComponent,
 ];
 
 @NgModule({
--- src/app/modules/detail/detail.routing.ts
+++ src/app/modules/detail/detail.routing.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from '@modules/detail';
+import { OrderComponent } from '@modules/detail/order';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'detail',
         children: [
             {
                 path: ':goodsId',
                 component: DetailComponent
             }
         ]
+    },
+    {
+        path: 'order',
+        component: OrderComponent
     }
 ];
 
