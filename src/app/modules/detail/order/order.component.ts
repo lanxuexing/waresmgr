3427cfc13334892133bace0726a90776066e5df4
lanxuexing <lanxuexing313wsr@163.com>
2020-03-15 13:53:23 +0800

    feat: rxjs calc totlePrice

--- src/app/modules/detail/order/order.component.ts
+++ src/app/modules/detail/order/order.component.ts
@@ -1,6 +1,8 @@
+import { Detail } from '@models/detail';
 import { DialogService } from '@services/dialog';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, combineLatest, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order',
@@ -11,6 +13,7 @@ import { Observable } from 'rxjs';
 export class OrderComponent implements OnInit {
   order$: Observable<object | null>;
   totalPrice$: Observable<number>;
+  count$ = new Subject<number>();
 
   constructor(
     private dialogService: DialogService
@@ -18,9 +21,24 @@ export class OrderComponent implements OnInit {
 
   ngOnInit(): void {
     this.order$ = this.dialogService.getDialogData();
+    // 单价
+    const unitPrice$ = this.order$.pipe(
+      map((item: {detail: Detail, count: number}) => item.detail.price)
+    );
+    // 数量
+    const amount$ = this.order$.pipe(
+      map((item: {detail: Detail, count: number}) => item.count)
+    );
+    const mergeCount$ = merge(amount$, this.count$);
+    // 总价
+    this.totalPrice$ = combineLatest([unitPrice$, mergeCount$]).pipe(
+      map(([price, amount]) => price * amount)
+    );
   }
 
   // 商品数量
-  handelCounterchange(count: number): void { }
+  handelCounterchange(count: number): void {
+    this.count$.next(count);
+  }
 
 }
